78ed744f4982eb4e4648c2be4dbc336a4b107e3a
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-07-01 11:15:57 -0400

    Updates `adorn_daa_data`
    Updates function `adorn_daa_data` to handle changes to `get_daa_data`:
    * Adds `include_coc` parameter that determines whether `categoryOptionCombo` data is cleaned and returned or removed from dataset. In case where `categoryOptionCombo` is not included, data is aggregated up to reflect non-inclusion.
    * Adds `d2_session` handle as parameter so that `datimutils::getDataElements` and `datimutils::getCatOptionCombos` can be used to adorn dataset.

--- R/adorn-daa-data.R
+++ R/adorn-daa-data.R
@@ -7,65 +7,95 @@
 #' as well as other information.
 #'
 #' @param df Dataframe containing DAA data indicator data to be adorned.
+#' @param d2_session R6 session object.
 #'
 #' @return Dataframe of DAA indicator data for both PEPFAR and the MOH as well
 #' as both discordance and concordance metrics.
 #'
-adorn_daa_data <- function(df) {
+adorn_daa_data <- function(df, include_coc = FALSE, d2_session = dynGet("d2_default_session", inherits = TRUE)) {
   # Returns null if delivered an empty dataset
   if (is.null(df)) {
     return(NULL)
   }
+
+  # Grab metadata for data elements
+  de_meta <-
+    datimutils::getDataElements(unique(df$data_element),
+                                fields = c("id", "name"),
+                                d2_session = d2_session) |>
+    dplyr::mutate(indicator = stringr::str_extract(name, "\\w*(?=\\s)"))
+
   # Cleans data and prepares it for export
   df <- df |>
+    # Recasts values as numeric
+    dplyr::mutate(value = as.numeric(`value`)) |>
+
     # Pivots MOH and PEPFAR data out into separate columns
-    tidyr::pivot_wider(names_from = .data$`Funding Mechanism`,
-                       values_from = .data$`Value`) %>%
+    tidyr::pivot_wider(names_from = `attribute_option_combo`,
+                       values_from = `value`) |>
 
     # Cleans Period data from the form `2018Oct` to `2019`
     dplyr::mutate(period =
-                    as.numeric(stringr::str_sub(.data$`Period`, 0, 4)) + 1) %>%
+                    as.numeric(stringr::str_sub(`period`, 0, 4)) + 1) |>
 
-    # Renames MOH and PEPFAR columns and converts them to numeric data types
-    dplyr::mutate(moh = as.numeric(.data$`mXjFJEexCHJ`)) %>%
-    dplyr::mutate(pepfar = as.numeric(.data$`t6dWOH7W5Ml`)) %>%
+    # Adorn indicator names
+    dplyr::left_join(de_meta, by = c("data_element" = "id")) |>
+    dplyr::mutate(indicator = ifelse(indicator == "TB_PREV" && period < 2020, "TB_PREV_LEGACY", indicator))
 
-    # Filtering out HTS_TST data from indicators V6hxDYUZFBq and BRalYZhcHpi
-    # to only FY2020 to prevent duplication
-    dplyr::filter((.data$Data %in% c("V6hxDYUZFBq", "BRalYZhcHpi"))
-                  & .data$period >= 2020 |
-                    !(.data$Data %in% c("V6hxDYUZFBq", "BRalYZhcHpi"))) %>%
+  if (include_coc == TRUE) {
+    # Grab metadata for category option combos
+    coc_metadata <-
+      datimutils::getCatOptionCombos(unique(df$category_option_combo),
+                                     fields = c("id", "name")) |>
+      dplyr::rename(coc_id = id, coc_name = name)
 
-    # TODO Filter this data out before the data call or figure
-    # out how to present it to the user effectively
-    # Filters out indicator LZbeWYZEkYL to prevent duplication of TB_PREV data
-    dplyr::filter(.data$Data != "LZbeWYZEkYL") %>%
+    # Adorn category option combo metadata
+    df <- df |>
+      dplyr::left_join(coc_metadata,
+                       by = c("category_option_combo" = "coc_id"),
+                       keep = FALSE) |>
 
-    # Generates human-readable indicator names
-    dplyr::mutate(Data = get_indicator_name(.data$Data)) %>%
+      # Summarise indicator data across Age & Age Agg
+      dplyr::group_by(org_unit, indicator, coc_name) |>
+      dplyr::mutate(moh = sum(`00100`), pepfar = sum(`00200`)) |>
+      dplyr::ungroup() |>
 
-    # Summarizes MOH and PEPFAR data up from coarse and fine disaggregates
-    dplyr::group_by(.data$Data, .data$`Organisation unit`, .data$period) %>%
-    dplyr::summarise(moh =
-                       sum(.data$moh, na.rm = any(!is.na(.data$moh))),
-                     pepfar =
-                       sum(.data$pepfar, na.rm = any(!is.na(.data$pepfar)))) %>%
-    dplyr::ungroup() %>%
+      # Creates summary data about reporting institutions and figures
+      dplyr::mutate(reported_by =
+                      ifelse(!is.na(moh),
+                             ifelse(!is.na(pepfar), "Both", "MOH"),
+                             ifelse(!is.na(pepfar), "PEPFAR", "Neither"))) |>
 
-    # Creates summary data about reporting institutions and figures
-    dplyr::mutate(reported_by =
-                    ifelse(!is.na(.data$moh),
-                           ifelse(!is.na(.data$pepfar), "Both", "MOH"),
-                           ifelse(!is.na(.data$pepfar),
-                                  "PEPFAR", "Neither"))) %>%
+      # Reorganizes table for export
+      dplyr::select(facilityuid = `org_unit`,
+                    indicator,
+                    categoryOptionCombo = `coc_name`,
+                    period,
+                    moh,
+                    pepfar,
+                    reported_by)
 
-    # Reorganizes table for export
-    dplyr::select(facilityuid = .data$`Organisation unit`,
-                  indicator = .data$`Data`,
-                  .data$period,
-                  .data$moh,
-                  .data$pepfar,
-                  .data$reported_by)
+  } else {
+    df <- df |>
+      # Summarise indicator data across Age & Age Agg
+      dplyr::group_by(org_unit, indicator, period) |>
+      dplyr::summarise(moh = sum(`00100`), pepfar = sum(`00200`)) |>
+      dplyr::ungroup() |>
+
+      # Creates summary data about reporting institutions and figures
+      dplyr::mutate(reported_by =
+                      ifelse(!is.na(moh),
+                             ifelse(!is.na(pepfar), "Both", "MOH"),
+                             ifelse(!is.na(pepfar), "PEPFAR", "Neither"))) |>
+
+      # Reorganizes table for export
+      dplyr::select(facilityuid = `org_unit`,
+                    indicator,
+                    period,
+                    moh,
+                    pepfar,
+                    reported_by)
+  }
 
   return(df)
 }
