cd87d3c0cf4f5c10cbe5d2927dcf667d9a7a2180
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-07-01 11:21:18 -0400

    Create data_integrity_check_files.R
    Creates `data-raw` file to pull data integrity check data.

---
+++ data-raw/data_integrity_check_files.R
@@ -0,0 +1,42 @@
+## code to prepare Data Integrity Check country datasets and Excel files goes here
+require(openxlsx)
+require(magrittr)
+
+folder <- "C:/Users/cnemarich002/OneDrive - Guidehouse/Documents/project_DAA/data_integrity_checks/"
+
+s3 <- paws::s3()
+aws_s3_bucket <- Sys.getenv("AWS_S3_BUCKET")
+
+nulls <- daa.analytics::get_s3_data(s3 = s3,
+                                    aws_s3_bucket = aws_s3_bucket,
+                                    dataset_name = "null_ids",
+                                    folder = "data-raw")
+duplicates <- daa.analytics::get_s3_data(s3 = s3,
+                                         aws_s3_bucket = aws_s3_bucket,
+                                         dataset_name = "duplicate_ids",
+                                         folder = "data-raw")
+
+countries <-
+  rbind(dplyr::select(nulls, country_name = "level3"),
+                   dplyr::select(duplicates, "country_name"))[[1]] |>
+  unique() |>
+  sort() |>
+  lapply(., function(x){
+    date <- base::format(Sys.time(), "%Y%m%d")
+    print(x)
+    file = paste0(folder, paste(date, x, "integrity_checks", sep = "_"), ".xlsx")
+    wb <- openxlsx::createWorkbook(title = paste(x, "Data Integrity Checks"))
+    country_nulls <- nulls[nulls$level3 == x, ]
+    country_duplicates <- duplicates[duplicates$country_name == x, ]
+    if (NROW(country_nulls) > 0) {
+      openxlsx::addWorksheet(wb = wb, sheet = "Null IDs")
+      openxlsx::writeData(wb = wb, sheet = "Null IDs",
+                          x = country_nulls)
+    }
+    if (NROW(country_duplicates) > 0) {
+      openxlsx::addWorksheet(wb = wb, sheet = "Duplicate IDs")
+      openxlsx::writeData(wb = wb, sheet = "Duplicate IDs",
+                          x = country_duplicates)
+    }
+    openxlsx::saveWorkbook(wb = wb, file = file, overwrite = TRUE)
+  })
