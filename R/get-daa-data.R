81ccf5a390908515a9f13d8d2c060907ad58a879
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-07-01 11:11:12 -0400

    Updates `get_daa_data`
    Updates `get_daa_data`:
    *Pulls from `dataValueSets` API endpoint instead of `analytics` endpoint. * Adds a `fiscal_year` parameter to filter which fiscal years should be returned.
    * Now returns `categoryOptionCombo` as a column and returns Age/Sex data by default.
    * Adds `getDataSetUids` and `getDataValueSets` to handle this transition.

--- R/get-daa-data.R
+++ R/get-daa-data.R
@@ -1,65 +1,115 @@
 #' @export
+#' @title Get Data Set UIDs
+#'
+#' @param fiscal_year List of fiscal years starting in October.
+#'
+#' @return Filtered dataframe of fiscal years and dataSets
+#' for DAA indicator data.
+#'
+getDataSetUids <- function(fiscal_year = NULL) {
+  dataSetUids <- data.frame(
+    fiscal_year = c("2017",
+                    "2018",
+                    "2019",
+                    "2020",
+                    "2021",
+                    "2022"),
+    dataSet = c("FJrq7T5emEh",
+                "sfk9cyQSUyi",
+                "OBhi1PUW3OL",
+                "QSodwF4YG9a",
+                "U7qYX49krHK",
+                "RGDmmG5taRt"
+    ))
+
+  # Filter dataSetUids if fiscal years provided
+  if (!is.null(fiscal_year)) {
+    dataSetUids <- dataSetUids[dataSetUids$fiscal_year %in% fiscal_year, ]
+  }
+
+  # Provide warning if no valid fiscal years provided
+  if (NROW(dataSetUids) == 0) {
+    warning("No dataSet UIDs available for the given fiscal years!")
+  }
+
+  # Return dataSetUids object
+  dataSetUids
+}
+
+#' @export
+#' @title Get Data Value Sets
+#'
+#' @param parameters Dataframe of key-value pairs that will be taken as parameters of the API call.
+#' @param d2_session R6 session object
+#'
+#' @return Dataframe with six columns.
+#'
+getDataValueSets <- function(parameters, d2_session  = dynGet("d2_default_session", inherits = TRUE)) {
+  parameters <- stringr::str_c(parameters$key, parameters$value, sep = "=", collapse = "&")
+
+  url <- paste0(d2_session$base_url, "api/dataValueSets.json?", parameters, "&paging=false")
+
+  interactive_print(url)
+
+  df <- httr::GET(url, httr::timeout(600), handle = d2_session$handle) |>
+    httr::content("text") |>
+    jsonlite::fromJSON() |>
+    purrr::pluck("dataValues") |>
+    dplyr::rename(data_element = "dataElement",
+                  org_unit = "orgUnit",
+                  category_option_combo = "categoryOptionCombo",
+                  attribute_option_combo = "attributeOptionCombo",
+                  stored_by = "storedBy",
+                  last_updated = "lastUpdated")
+
+  # Returns null if API returns nothing
+  if (is.null(df)) {
+    return(NULL)
+  }
+  # Returns dataframe
+  df
+}
+
+#' @export
 #' @title Get DAA Indicator Data
 #'
 #' @description
 #' Fetches DAA indicator data for both PEPFAR and the MOH partner for a single
 #' country.
 #'
-#' @param ou_uid UID for the Operating Unit whose data is being queried.
+#' @param ou_uid UIDs for the Operating Units whose data are being queried.
+#' @param fiscal_year Fiscal years for which data should be gathered.
 #' @param d2_session DHIS2 Session id for the DATIM session.
 #'
 #' @return Dataframe of unadorned PEPFAR and the MOH DAA indicator data.
 #'
-get_daa_data <- function(ou_uid, d2_session) {
+get_daa_data <- function(ou_uid, fiscal_year, d2_session = dynGet("d2_default_session", inherits = TRUE)) {
 
-  indicator_uids <- daa.analytics::daa_indicators$uid
+  interactive_print("Getting data for the following Operating Units:")
+  interactive_print(datimutils::getOrgUnits(ou_uid))
 
-  period_list <-
-    paste(paste0(2017:(daa.analytics::current_fiscal_year()), "Oct"),
-          collapse = ";")
+  dataSetUids <- getDataSetUids(fiscal_year)
 
-  # TODO Make this into a tryCatch to future proof against large OUs timing out
-  # Breaks the query into multiple parts for Nigeria to prevent timeout
-  df <- tryCatch({
-      datimutils::getAnalytics(
-        paste0("dimension=SH885jaRe0o:mXjFJEexCHJ;t6dWOH7W5Ml&",
-               "displayProperty=SHORTNAME&outputIdScheme=UID"),
-        dx = paste(indicator_uids, collapse = ";"),
-        pe = period_list,
-        ou = paste0("OU_GROUP-POHZmzofoVx;", ou_uid),
-        d2_session = d2_session,
-        retry = 2
-      )
-    },
-    # If error is thrown, then try again splitting query into multiple parts.
-    error = function(e) {
-      # TODO log this in a log file.
-      # Prints an error message.
-      print(
-        paste0("Error: Timeout was reached after 60 seconds with 0 bytes",
-               "received. Trying again with query broken into smaller chunks.")
-      )
-      # Pulls data for each indicator as an individual query to shrink size
-      indicator_uids %>%
-        # Makes queries for each group of indicators
-        lapply(function(x) {
-          datimutils::getAnalytics(
-            paste0("dimension=SH885jaRe0o:mXjFJEexCHJ;t6dWOH7W5Ml&",
-                   "displayProperty=SHORTNAME&outputIdScheme=UID"),
-            dx = x,
-            pe = period_list,
-            ou = paste0("OU_GROUP-POHZmzofoVx;", ou_uid),
-            d2_session = d2_session
-          )
-        }) %>%
-        # Binds all of the component dataframes together
-        dplyr::bind_rows()
-    }
-  )
+  df <- lapply(fiscal_year, function(x) {
+    interactive_print(paste0("Now getting data for fiscal year ", x, "."))
+    getDataValueSets(parameters = rbind(data.frame(key = "dataSet",
+                                                   value = dataSetUids$dataSet[dataSetUids$fiscal_year == x]),
+                                        data.frame(key = "orgUnit", value = ou_uid),
+                                        data.frame(key = "period", value = paste0(x - 1, "Oct")),
+                                        data.frame(key = c("children",
+                                                           "categoryOptionComboIdScheme",
+                                                           "includeDeleted"),
+                                                   value = c("true",
+                                                             "code",
+                                                             "false"))),
+                     d2_session = d2_session)
+  }) |>
+    dplyr::bind_rows()
+
   # Returns null if API returns nothing
   if (is.null(df)) {
     return(NULL)
   }
   # Returns dataframe
-  return(df)
+  df
 }
