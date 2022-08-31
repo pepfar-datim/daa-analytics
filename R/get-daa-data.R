f1526971791fc86294dbd8dbacf956ce097b9fc0
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-08-31 10:15:45 -0400

    Replace custom function with `datimutils` version
    Replaces and removes the custom function `get_data_value_sets` with the usage of `datimutils::getDataValueSets`

--- R/get-daa-data.R
+++ R/get-daa-data.R
@@ -36,41 +36,6 @@ get_dataset_uids <- function(fiscal_year = NULL) {
   dataset_uids
 }
 
-#' @export
-#' @title Get Data Value Sets
-#'
-#' @param parameters Dataframe of key-value pairs that
-#' will be taken as parameters of the API call.
-#' @param d2_session R6 session object
-#'
-#' @return Dataframe with six columns.
-#'
-get_data_value_sets <- function(parameters,
-                                d2_session = dynGet("d2_default_session",
-                                                    inherits = TRUE)) {
-  parameters <- stringr::str_c(parameters$key,
-                               parameters$value,
-                               sep = "=",
-                               collapse = "&")
-
-  url <- paste0(d2_session$base_url,
-                "api/dataValueSets.json?",
-                parameters,
-                "&paging=false")
-
-  interactive_print(url)
-
-  httr::GET(url, httr::timeout(600), handle = d2_session$handle) |>
-    httr::content("text") |>
-    jsonlite::fromJSON() |>
-    purrr::pluck("dataValues") |>
-    dplyr::rename(data_element = "dataElement",
-                  org_unit = "orgUnit",
-                  category_option_combo = "categoryOptionCombo",
-                  attribute_option_combo = "attributeOptionCombo",
-                  stored_by = "storedBy",
-                  last_updated = "lastUpdated")
-}
 
 #' @export
 #' @title Get DAA Indicator Data
@@ -90,26 +55,23 @@ get_daa_data <- function(ou_uid,
                          d2_session = dynGet("d2_default_session",
                                              inherits = TRUE)) {
 
-  interactive_print("Getting data for the following Operating Units:")
-  interactive_print(datimutils::getOrgUnits(ou_uid))
-
-  dataset_uids <- get_dataset_uids(fiscal_year)
-
-  lapply(fiscal_year, function(x) {
-    interactive_print(paste0("Now getting data for fiscal year ", x, "."))
-    get_data_value_sets(
-      parameters = rbind(
-        data.frame(key = "dataSet",
-                   value = dataset_uids$dataSet[dataset_uids$fiscal_year == x]),
-        data.frame(key = "orgUnit", value = ou_uid),
-        data.frame(key = "period", value = paste0(x - 1, "Oct")),
-        data.frame(key = c("children",
-                           "categoryOptionComboIdScheme",
-                           "includeDeleted"),
-                   value = c("true",
-                             "code",
-                             "false"))),
-      d2_session = d2_session)
-  }) |>
-    dplyr::bind_rows()
+  datimutils::getDataValueSets(
+    variable_keys = c("dataSet",
+                      "orgUnit",
+                      "period",
+                      "children",
+                      "categoryOptionComboIdScheme",
+                      "includeDeleted"),
+    variable_values = c(get_dataset_uids(fiscal_year)$dataSet,
+                        ou_uid,
+                        paste0(fiscal_year - 1, "Oct"),
+                        "true",
+                        "code",
+                        "false"),
+    d2_session = d2_session)[, c(data_element,
+                                 period,
+                                 org_unit,
+                                 category_option_combo,
+                                 attribute_option_combo,
+                                 value)]
 }
